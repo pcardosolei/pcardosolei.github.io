200896c518ee3321af569a6593ebac11c7f737ff
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2017-07-23 16:57:20 +0100

    starting to create my interactive website

--- js/test.js
+++ js/test.js
@@ -1,23 +1,54 @@
-var stage = new PIXI.Container(),
-    renderer = PIXI.autoDetectRenderer(256, 256);
+
+//Aliases
+var Container = PIXI.Container,
+    autoDetectRenderer = PIXI.autoDetectRenderer,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    text = PIXI.text,
+    Sprite = PIXI.Sprite;
+
+//Create a Pixi stage and renderer and add the
+//renderer.view to the DOM
+var stage = new Container(),
+    renderer = autoDetectRenderer(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.view);
 
-//Use Pixi's built-in `loader` object to load an image
-PIXI.loader
-  .add("img/cat.png")
-  .load(setup);
 
-//This `setup` function will run when the image has loaded
+
+/*
+  SOUND
+*/
+//Load the sounds
+sounds.load([
+  "sounds/57583__digifishmusic__palm-cove-waves.wav"
+]);
+
+//Assign the callback function that should run
+//when the sounds have loaded
+sounds.whenLoaded = setup;
+
 function setup() {
+  //Initialize sounds here
+  var music = sounds["sounds/57583__digifishmusic__palm-cove-waves.wav"];
 
-  //Create the `cat` sprite from the texture
-  var cat = new PIXI.Sprite(
-    PIXI.loader.resources["img/cat.png"].texture
-  );
+  let myname = new PIXI.Text('Paulo Cardoso',{fontFamily : 'emizfont', fontSize: 48, fill : 0xFFFFFF, align : 'center'});
+  myname.tint = 0xFFFFFF;
+  myname.anchor.set(0.5,0.5);
+  myname.position.set( window.innerWidth / 2 , window.innerHeight / 2);
+  stage.addChild(myname);
 
-  //Add the cat to the stage
-  stage.addChild(cat);
+  let changesSoon = new PIXI.Text('Website is being built. Expect changes soon.',{fontFamily : 'Arial', fontSize: 24, fill : 0xFFFFFF, align : 'center'});
+  changesSoon.tint = 0xFFFFFF;
+  changesSoon.anchor.set(0.5,0.5);
+  changesSoon.position.set( window.innerWidth / 2 , window.innerHeight - 30);
+  stage.addChild(changesSoon);
 
-  //Render the stage
+
+  //Make the sound loop
+  music.loop = true;
+  //Play the sound
+  music.play();
+
   renderer.render(stage);
 }
+
