838bf25366e0cce923bced3205184bfb420616ce
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2018-02-04 00:39:40 +0000

    uploading more content

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-	$('#fullpage').fullpage();
+	$('#fullpage').fullpage({
+      css3: true,
+      menu: true,
+      anchors:['Intro','About Me','Career','Volunteer','Internacional','Contact'], /* FOR THE MENU WHEN YOU ARE GOING TO PUT IT */
+      sectionsColor: ['#125485', '#3A2E39', '#6B818C', '#2E86AB', '#E3D0D8']
+  });
 });
