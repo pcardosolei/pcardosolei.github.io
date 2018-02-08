7a96fb5bb33cfedd652932854fe25de6d18d99ef
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2018-02-08 00:23:28 +0000

    finishing up some stuff

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
       css3: true,
       menu: true,
       anchors:['Intro','About Me','Career','Volunteer','Internacional','Contact'], /* FOR THE MENU WHEN YOU ARE GOING TO PUT IT */
-      sectionsColor: ['#125485', '#3A2E39', '#6B818C', '#2E86AB', '#191D32', '#0A0908']
+      sectionsColor: ['#125485', '#3A2E39', '#6B818C', '#2E86AB', '#191D32', '#0A0908'],
+      controlArrows: false,
+      slidesNavigation: true
   });
 });
