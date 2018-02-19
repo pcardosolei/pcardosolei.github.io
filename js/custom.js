c9f1416730604d7cf73fe983dc024b0bbdf65bd1
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2018-02-19 21:52:11 +0000

    adding info , fixing some css

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,8 @@ $(document).ready(function() {
       sectionsColor: ['#125485', '#3A2E39', '#6B818C', '#2E86AB', '#191D32', '#0A0908'],
       controlArrows: false,
       slidesNavigation: true,
-      navigation: true,
       menu: '#myMenu',
       anchors:['Intro','AboutMe','Career','Volunteer','Internacional','Contact'], /* FOR THE MENU WHEN YOU ARE GOING TO PUT IT */
   });
 });
-	
+
