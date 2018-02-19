918acbe06b181d1d3a667926272377dcb608d701
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2018-02-19 01:51:24 +0000

    adding menu, more information

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
       css3: true,
-      menu: true,
-      anchors:['Intro','About Me','Career','Volunteer','Internacional','Contact'], /* FOR THE MENU WHEN YOU ARE GOING TO PUT IT */
       sectionsColor: ['#125485', '#3A2E39', '#6B818C', '#2E86AB', '#191D32', '#0A0908'],
       controlArrows: false,
-      slidesNavigation: true
+      slidesNavigation: true,
+      navigation: true,
+      menu: '#myMenu',
+      anchors:['Intro','AboutMe','Career','Volunteer','Internacional','Contact'], /* FOR THE MENU WHEN YOU ARE GOING TO PUT IT */
   });
 });
 	
