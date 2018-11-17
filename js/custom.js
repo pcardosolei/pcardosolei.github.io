6a1a306bd2518a2c058e60119251a18619733de1
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2018-11-18 00:31:04 +0100

    adding better logs and changing some css

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,7 @@
 $(document).ready(function() {
-    $("#personal-info").click(function(){
-        alert("p-button");
-    }); 
-    $("#work-info").click(function(){
-        alert("w-button");
-    }); 
-    $("#volunteer-info").click(function(){
-        alert("v-button");
-    }); 
-    $("#contact-info").click(function(){
-        alert("button");
-    }); 
+    $("#personal-info").click(function(){}); 
+    $("#work-info").click(function(){}); 
+    $("#volunteer-info").click(function(){}); 
+    $("#contact-info").click(function(){}); 
 });
 
